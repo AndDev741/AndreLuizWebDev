54c7a03023255accd461503b307ba8b4b3b4accd
AndDev741 <anddesenvolved@gmail.com>
2025-01-30 16:13:44 -0300

    feat: Footer component translation optimized

--- src/PagesEn/Components/Footer.js
+++ src/PagesEn/Components/Footer.js
@@ -1,13 +1,15 @@
 import { Element, Link } from "react-scroll";
 import { FaRegHandPointUp } from 'react-icons/fa'
+import { useTranslation } from "react-i18next";
 
 export function Footer(){
+    const {t} = useTranslation();
     return(
         <Element className="flex flex-col md:flex-row items-center justify-center md:justify-between h-[20vh] border-t-2 border-black font-pFont">
-            <h2 className="text-xl sm:text-2xl md:text-3xl md:ml-12 mt-4 sm:mt-0">©André Luiz Digital solutions, 2023</h2>
+            <h2 className="text-xl sm:text-2xl md:text-3xl md:ml-12 mt-4 sm:mt-0">©André Luiz {t('Digital Solutions')}, 2023</h2>
             <Link to="home" smooth={true} className="mt-5 lg:mt-0 cursor-pointer md:mr-12 flex flex-col justify-end items-center">
                 <FaRegHandPointUp className="text-[40px]"/>
-                <p className="text-2xl">Go to top</p>
+                <p className="text-2xl">{t("Go to top")}</p>
             </Link>
             
         </Element>
